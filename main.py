9f68a21d01cb60e757dfe5a08752fb7c76a468dd
kumar0384 <pavankumar.nr@gmail.com>
2026-03-04 06:52:28 -0600

    Refactor main.py for weather notifications
    Removed birthday notification code and added weather notification functionality.

--- main.py
+++ main.py
@@ -6,33 +6,75 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
-import pandas
-import random
+# from datetime import datetime
+# import pandas
+# import random
+# import smtplib
+# import os
+
+# # import os and use it to get the Github repository secrets
+# MY_EMAIL = os.environ.get("MY_EMAIL")
+# MY_PASSWORD = os.environ.get("MY_PASSWORD")
+
+# today = datetime.now()
+# today_tuple = (today.month, today.day)
+
+# data = pandas.read_csv("birthdays.csv")
+# birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
+# if today_tuple in birthdays_dict:
+#     birthday_person = birthdays_dict[today_tuple]
+#     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+#     with open(file_path) as letter_file:
+#         contents = letter_file.read()
+#         contents = contents.replace("[NAME]", birthday_person["name"])
+
+#     with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+#         connection.starttls()
+#         connection.login(MY_EMAIL, MY_PASSWORD)
+#         connection.sendmail(
+#             from_addr=MY_EMAIL,
+#             to_addrs=birthday_person["email"],
+#             msg=f"Subject:Happy Birthday!\n\n{contents}"
+#         )
+
+import requests
 import smtplib
+from twilio.rest import Client
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+API_KEY = os.environ.get("API_KEY")
+OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast?"
+params = {
+    "lat" : 39.952583,
+    "lon" : -75.165222,
+    "appid" : API_KEY,
+    "cnt" : 4,
+}
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+TWILIO_FAILSAFE = "TXRTAJ88NU5K1QMM8GAQVT1L"
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+
+#URL = f"http://api.openweathermap.org/data/2.5/weather?q=conroe,texas&appid={API_KEY}"
+
+response = requests.get(url=OWM_ENDPOINT,params=params)
+print(response.raise_for_status())
+
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = int(hour_data["weather"][0]["id"])
+    if condition_code <700:
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_="whatsapp:+14155238886",
+        body="It is going to rain today!. Carry an umbrella!",
+        to="whatsapp:+19367665010",
+    )
+
+    print(message.body)
